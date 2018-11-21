13ea0ba1f22b67bce1331a8a96b6fff7b374099b
gguo <gguo@movoto.com>
2018-11-21 09:32:07 -0800

    added more importer

--- module_importer.py
+++ module_importer.py
@@ -3,9 +3,13 @@ sys.path.insert(0, './utils')
 
 from utils import *
 from module1 import disp1
+import utils.module2
+from utils.module2 import display
 
 disp1()
 # disp2()
 
 module2.display() # pylint: disable=E0602
 mmm3.displayM() # pylint: disable=E0602
+utils.module2.display();
+display()
