40be238c5f35d327bb0c334bd5792b006290fe05
geforcesong <georgeguo@outlook.com>
2019-01-15 14:16:19 -0800

    added decorator test

---
+++ decoratortest.py
@@ -0,0 +1,27 @@
+def simple(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+def log(text, num):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('call (%s, %s) %s():' % (text, num, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('12312', 456)
+def is_odd(n):
+    return n % 2 == 1
+
+
+# @simple
+# def is_odd(n):
+#     return n % 2 == 1
+
+L = list(filter(is_odd, range(1, 20)))
+
+
+print(L)
