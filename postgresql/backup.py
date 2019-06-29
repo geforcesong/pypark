b9d779052348782752388182b2656b81d37e6af1
geforcesong <georgeguo@outlook.com>
2019-06-29 10:13:46 -0700

    updated

--- postgresql/backup.py
+++ postgresql/backup.py
@@ -1,5 +1,5 @@
 import subprocess
-command = 'pg_dump -h {0} -d {1} -U {2} -f ~/Documents/table3.dmp'.format(
+command = 'pg_dump -h {0} -d {1} -U {2} -f ./postgresql/baks/table3.dmp'.format(
     'localhost', 'Student', 'george')
 popen = subprocess.Popen(
     command, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
