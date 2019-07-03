4a56b81a52b32ac03046613ea698bc5f7770dcc5
geforcesong <georgeguo@outlook.com>
2019-07-03 13:49:27 -0700

    updated

--- tmptests/uservoicetest.py
+++ tmptests/uservoicetest.py
@@ -1,12 +1,12 @@
 
 import uservoice
 
-SUBDOMAIN_NAME = 'uservoice'
+SUBDOMAIN_NAME = 'movoto'
 API_KEY = 'KbUgMTKwOovLVcNnvI1DQ'
 API_SECRET = 'QZFsHiTVaTjRAdIvMPw6VI2iOASCXrIGvOpKcxWnLQ'
 
 client = uservoice.Client(SUBDOMAIN_NAME, API_KEY, API_SECRET)
 with client.login_as_owner() as owner:
-    user = owner.get("/api/v1/users/current")
+    user = owner.get('/api/v1/custom_fields.json')
     print(user)
 
